f3562aeefc9cb3462103bc6ac48bf1ff8ab47a5f
Sergio Erazo <sergioorteera@unicauca.edu.co>
2025-10-27 01:42:42 -0500

    refactor: Replace StrictMode with Fragment in main component and add JSDoc comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from "react";
+import { Fragment } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import { AppRouter } from "./routes/app-router";
+import "./index.css";
 
+/**
+ * Root component
+ * @returns {JSX.Element} JSX element
+ */
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <Fragment>
     <AppRouter />
-  </StrictMode>
+  </Fragment>
 );
